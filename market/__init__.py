561a9cc48048d6d4b7d0c5b592d84a2b106cfaec
Venkat <venkatbs500@gmail.com>
2024-11-05 17:24:55 +0530

    added new stuff

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/venky/Desktop/Market/market/market.db'
+app.config['SECRET_KEY'] = '2391ea8fa7b17029d0a01a7f'
 db = SQLAlchemy(app)
 
 from market import routes
